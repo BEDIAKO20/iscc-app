1c62bfbde9f7afc5161cbd270787e69b51d2f2d1
BEDIAKO20 <83319947+BEDIAKO20@users.noreply.github.com>
2023-12-07 09:26:24 +0000

    addSome text to the About Page

--- src/components/WhoWeAre/Whoweare.js
+++ src/components/WhoWeAre/Whoweare.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import "../WhoWeAre/whoweare.css"
+import groupImage from "../../assets/groupImage.png"
 
 function Whoweare() {
   return (
     <>
 <div className=''>
-
+<div className="AboutPagText-Box">
+<div className="About-text ">
+  <h1 className='About-text'>About <span className='us-Text'>Us</span></h1>
+</div>
+</div>
+<div className='image-Box'>
+  <img src={groupImage} className='groupImage'/>
+</div>
 </div>
 
     </>
